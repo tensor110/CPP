aef06a8e9cbadb6e10bfa3aabb41242c7aecbceb
Maheswar Mahapatra <maheswar2110@gmail.com>
2023-08-23 21:07:11 +0530

    Input and output

---
+++ 2ip&op.cpp
@@ -0,0 +1,12 @@
+// Input and output
+#include <iostream>
+using namespace std;
+int main(){
+    int am1,am2;
+    cout<<"Amount1: ";
+    cin>>am1;
+    cout<<"Amount2: ";
+    cin>>am2;
+    cout<<"Total amount: ";
+    cout<<am1+am2;
+}
