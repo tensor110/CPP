ecda32f243f0cd116011b411f11e05a4cd3301ca
Maheswar Mahapatra <maheswar2110@gmail.com>
2023-08-23 21:20:48 +0530

    Loops

---
+++ 4loops.cpp
@@ -0,0 +1,19 @@
+// Loops
+// For loop
+// for(initialisation;condition;update){
+//     body;
+// }
+
+// While loop 
+// initialisation;
+// while(condition){
+//     body;
+//     update;
+// }
+
+// Do-While loop 
+// initialisation;
+// do{
+//     body;
+//     update;
+// }while(condition)
