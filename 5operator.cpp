a4a6830589822ebdda3882cab840d3d1db5de88f
Maheswar Mahapatra <maheswar2110@gmail.com>
2023-08-23 21:31:14 +0530

    Bitwise Operator

---
+++ 5operator.cpp
@@ -0,0 +1,9 @@
+// Bitwise Operator
+// AND Operator: &
+// OR Operator: |
+// XOR Operator: ^
+// Ones Operator: ~
+// Left Shift Operator: <<
+// Right Shift Operator: >>
+// a<<n -> a* 2^n
+// a>>n -> a/ 2^n
